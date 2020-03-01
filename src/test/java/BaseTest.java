5d28bae28c47dccaa246410dd3575ed6b5642029
yuliyaklepova <yanju@tut.by>
2020-03-02 02:22:01 +0300

    upd basetest & test for locators

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     protected WebDriver driver;
@@ -12,6 +14,8 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://dev.integrivideo.com/demo/chat/new");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
     }
 
     @AfterMethod(alwaysRun = true)
